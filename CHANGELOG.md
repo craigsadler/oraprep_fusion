86685fe9e44b14f0dcf1bcb78d37e9668f23552c
Craig Sadler <craig.sadler@gmail.com>
2014-05-30 09:42:14 -0700

    Initial

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+oraprep_fusion CHANGELOG
+========================
+
+This file is used to list changes made in each version of the oraprep_fusion cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of oraprep_fusion
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,117 @@
+#
+# Cookbook Name:: oraprep_fusion
+# Recipe:: default
+#
+# Copyright 2014, San Diego State University
+#
+# All rights reserved - Do Not Redistribute
+#
+ 
+include_recipe 'sysctl'
+
+sysctl_param  'kernel.shmmax' do
+   value '4294967295'
+end
+
+yum_package "binutils" do
+  action :install
+end
+
+yum_package "compat-libstdc++-33" do
+  action :install
+end
+
+yum_package "compat-libstdc++-33" do
+  arch "i386"
+  action :install
+end
+
+yum_package "elfutils-libelf" do
+  action :install
+end
+
+yum_package "gcc" do
+  action :install
+end
+
+yum_package "glibc" do
+  action :install
+end
+
+yum_package "glibc" do
+  action :install
+  arch "i686"
+end
+ 
+yum_package "glibc-common" do
+  action :install
+end
+
+yum_package "glibc-devel" do
+  action :install
+end
+
+yum_package "glibc-devel" do
+  action :install
+  arch "i386"
+end
+
+yum_package "libaio" do
+  action :install
+end
+
+yum_package "libaio" do
+    action :install
+    arch "i386"
+end
+
+yum_package "libaio-devel" do
+  action :install
+end
+
+yum_package "libgcc" do
+  action :install
+end
+
+yum_package "libgcc" do
+    action :install
+    arch "i386"
+end
+
+yum_package "libstdc++" do
+  action :install
+end
+
+yum_package "libstdc++" do
+    action :install
+    arch "i386"
+end
+ 
+yum_package "libstdc++-devel" do
+  action :install
+end
+ 
+yum_package "make" do
+  action :install
+end
+
+yum_package "openmotif" do
+  action :install
+end
+
+yum_package "openmotif" do
+    action :install
+    arch "i386"
+end
+ 
+yum_package "redhat-lsb" do
+  action :install
+end
+ 
+yum_package "sysstat" do
+  action :install
+end
+
+yum_package "xorg-x11-utils" do
+  action :install
+end
