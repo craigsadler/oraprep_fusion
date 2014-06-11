89fec0ad2ae219eb032e6530e73c4021db7035fd
Craig Sadler <craig.sadler@gmail.com>
2014-06-11 09:53:40 -0700

    Added bash

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -1,7 +1,7 @@
 ## Settings specific to the Oracle user.
 default[:oraprep_fusion][:user][:uid] = 500 
 default[:oraprep_fusion][:user][:gid] = 502 
-default[:oraprep_fusion][:user][:shell] = '/bin/ksh'
+default[:oraprep_fusion][:user][:shell] = '/bin/bash'
 default[:oraprep_fusion][:user][:sup_grps] = {'dba' => 503 }
 default[:oraprep_fusion][:user][:pw_set] = false
 default[:oraprep_fusion][:user][:edb] = 'oracle'
